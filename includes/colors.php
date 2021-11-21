a76917ba2735eafa8aacd583a26f391564b4dac4
Marek_p <marek.mnp@seznam.cz>
2021-11-21 15:01:06 +0100

    Add files via upload

---
+++ includes/colors.php
@@ -0,0 +1,96 @@
+<?php
+
+$backgroundcolor = "#fff";
+$cardbackground = "rgb(0, 153, 255)";
+$textcolor = "#fff";
+
+?>
+<style>
+    body {
+        background: <?php echo $backgroundcolor ?>;
+    }
+    .main .card {
+        background: <?php echo $cardbackground ?>;
+    }
+    .main .undertabs {
+        background: <?php echo $cardbackground ?>;
+    }
+    .main .undertabs2 {
+        background: <?php echo $cardbackground ?>;
+    }
+    .main table {
+        background: <?php echo $cardbackground ?>;
+    }
+    .main .infocard {
+        background: <?php echo $cardbackground ?>;
+    }
+    .main .settings_card {
+        background: <?php echo $cardbackground ?>;
+    }
+    .main .content form {
+        background: <?php echo $cardbackground ?>;
+    }
+    .main h1 {
+        color: <?php echo $textcolor ?>
+    }
+    .main h2 {
+        color: <?php echo $textcolor ?>
+    }
+    .main h3 {
+        color: <?php echo $textcolor ?>
+    }
+    .main h4 {
+        color: <?php echo $textcolor ?>
+    }
+    .main h5 {
+        color: <?php echo $textcolor ?>
+    }
+    .main h6 {
+        color: <?php echo $textcolor ?>
+    }
+    .main p {
+        color: <?php echo $textcolor ?>
+    }
+    .main a {
+        color: <?php echo $textcolor ?>
+    }
+    .main input {
+        color: <?php echo $textcolor ?>
+    }
+    .main button {
+        color: <?php echo $textcolor ?>
+    }
+    .main .card h1 {
+        color: <?php echo $textcolor ?>
+    }
+    .main .card h2 {
+        color: <?php echo $textcolor ?>
+    }
+    .main .card p {
+        color: <?php echo $textcolor ?>
+    }
+    .main table {
+        color: <?php echo $textcolor ?>
+    }
+    .sidenav .text1 {
+        color: <?php echo $textcolor ?>
+    }
+    .sidenav .text2 {
+        color: <?php echo $textcolor ?>
+    }
+    .header h1 {
+        color: <?php echo $textcolor ?>
+    }
+    .header p {
+        color: <?php echo $textcolor ?>
+    }
+    .header a {
+        color: <?php echo $textcolor ?>
+    }
+    .header i {
+        color: <?php echo $textcolor ?>
+    }
+    .sidenav i {
+        color: <?php echo $textcolor ?>
+    }
+</style>
