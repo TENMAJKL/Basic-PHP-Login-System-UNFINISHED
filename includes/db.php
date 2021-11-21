b4fa26aed6d9589b063630d25f7c59b67dcb5a31
Marek_p <marek.mnp@seznam.cz>
2021-11-21 15:03:05 +0100

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,6 +1,6 @@
 <?php // GalaxyCode Panel v1.0 by Marek_p
-$db_hostname = "mysql.hostnow.cz:3306";
-$db_username = "sql764_galaxycode";
-$db_password = "Heslo324";
-$db_database = "sql764_galaxycode";
+$db_hostname = "x";
+$db_username = "x";
+$db_password = "x";
+$db_database = "x";
 ?>
