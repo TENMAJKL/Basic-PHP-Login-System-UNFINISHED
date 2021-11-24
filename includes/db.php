f1c9a7c63cc710e5643653d4c83fa50cbad61163
Marek_p <marek.mnp@seznam.cz>
2021-11-24 17:10:27 +0100

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,6 +1,24 @@
 <?php // GalaxyCode Panel v1.0 by Marek_p
-$db_hostname = "x";
-$db_username = "x";
-$db_password = "x";
-$db_database = "x";
+
+// Připojení k MySQL databázi pro ukládání dat o oživatelích
+
+// #########################
+$db_hostname = "x"; // Hostname vaší MySQL databáze, například localhost:3306
+$db_username = "x"; // Uživatelské jméno účtu MySQL účtu s root právy pro správu databáze
+$db_password = "x"; // Heslo k účtu výše
+$db_database = "x"; // Jméno databáze (ne tabulky)
+// #########################
+
+// Doporučená struktura tabulky pro správné fungování:
+
+// Jméno tabulky "users", sloupce:
+
+  //  id         INT        A_I zaškrtnuté, množina libovolná, ale doporčuji min. 3
+  //  username   VARCHAR    množina libovolná, ale určuje maximální počet písmen, doporučuji min. 15
+  //  password   VARCHAR    množina libovolná, ale určuje maximální počet písmen, doporučuji min. 20
+  //  credit     INT        množina libivolná, ale určuje maxicmální počet cifer, doporučuji min. 5
+  //  rank       VARCHAR    množina libovolná, ale určuje maximální počet písmen, doporučuji min. 10
+
+// Discord podpora: https://dsc.gg/galaxycode  nebo  🔥𝕄𝕒𝕣𝕖𝕜_𝕡🔥#4775
+
 ?>
